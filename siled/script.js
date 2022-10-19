59cdfbcad21cfabef502f7b4b026e648b8526179
phucphan2001 <phucphan0708@gmail.com>
2022-10-19 21:20:08 +0700

    update

--- siled/script.js
+++ siled/script.js
@@ -6,3 +6,7 @@ document.getElementById('prev').onclick = function(){
     let list = document.querySelectorAll('.item');
     document.getElementById('slide').prepend(list[list.length -1]);
 }
+setInterval(function(){
+    let list = document.querySelectorAll('.item');
+    document.getElementById('slide').appendChild(list[0]);
+},8000)
